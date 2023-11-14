17cc546340e819014c68a7dab75cae398026e584
Cade Myers <Cade.Myers@selu.edu>
2023-11-14 20:58:48 +0000

    update NAMESPACE

---
+++ R/ggraphplot.R
@@ -0,0 +1,17 @@
+#' Graph relationship between sex and hindfoot length
+#' 
+#' Use sex and hindfoot length from surveys dataset to determine relationship
+#' Returns graphplot for sex versus hindfoot length
+#' 
+#' @param sex Sex of rodents
+#' @param hindfoot_length Hindfoot Length of rodents
+#' @return Returns gpp as graph plot between sex and hindfootlength 
+#' 
+#' @export
+
+
+
+graphplot_project <- function(data, sex, hindfoot_length){
+  gpp <- ggplot(data = surveys, mapping = aes(x = sex, y = hindfoot_length)) + geom_col()  
+  return(gpp)
+}
